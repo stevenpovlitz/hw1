cdcf1e07617dbe943a66f40aa546bf94dea3bb9b
Steven <steven.povlitz@gmail.com>
2017-09-21 19:24:51 -0400

    classification appears to be working

---
+++ hw1-alt.js
@@ -0,0 +1,92 @@
+var fs = require('fs');
+var euclidean = require( 'compute-euclidean-distance' );
+
+
+var trainingPath = 'spam_train.csv';
+var testPath = 'spam_test.csv';
+
+// synchronously read in and return the contents of a CSV file located
+// in the same file
+var readSync = function(path) {
+  var dataarr = []
+  var fileContents = fs.readFileSync(path);//expects 'data.csv' format
+  var lines = fileContents.toString().split('\n');
+
+  for (var i = 0; i < lines.length; i++) {
+    dataarr.push(lines[i].toString().split(','));
+  }
+
+  return (dataarr);
+}
+
+// determine the euclidean distances from all records in file1 to
+// record1, then push this distance to an array that will be returned
+// beware, pushing an empty first val to avoid off by one error when
+// comparing distances to a csv file's array
+var calcDistance = function(file1, record1) {
+  var distances = []; // will be returned
+  for (var i = 1; i < file1.length; i++) {
+    distances.push([i, euclidean(file1[i], record1)]);
+  }
+  return distances;
+}
+
+var sortDistance = function(a, b) {
+  return a[1] - b[1];
+}
+
+// based on a kval and a sorted distances array and the classification,
+// chose which classification we should get
+var voteClassification = function(trainingData, distances, kval){
+  if (kval % 2 == 0) {
+    console.log('WARNING: KVAL IS EVEN');
+  }
+  var spamvotes = 0;
+  var notspamvotes = 0;
+  for (var i = 1; i <= kval; i++) {
+    var temp = parseInt(trainingData[distances[i][0]][trainingData[0].length-1]);
+    // console.log("temp is: " + (temp));
+    if (temp === 0) {
+      notspamvotes++;
+    } else {
+      spamvotes++;
+    }
+  }
+  if (spamvotes < notspamvotes) {
+    // console.log('not spam');
+    return 0;
+  } else if (spamvotes > notspamvotes) {
+    // console.log('spam');
+    return 1;
+  } else {
+    // console.log('err - even kval and same votes');
+    return 2;
+  }
+}
+
+var classifyWholeSet = function(testData, trainingData, kval) {
+  console.log('each \'=\' is 10% completion');
+  process.stdout.write('(');
+  var spam = 0;
+  var notspam = 0;
+  for (var i = 1; i < trainingData.length; i++) {
+    var distances = calcDistance(testData, trainingData[i]);
+    distances.sort(sortDistance);
+    var tempresult = voteClassification(trainingData, distances, kval);
+    if (tempresult == 0) {
+      notspam++;
+    } else {
+      spam++;
+    }
+    if (i % parseInt(trainingData.length / 10) == 0) {
+      process.stdout.write('=');
+    }
+  }
+  process.stdout.write(')\n');
+  console.log("total spam: " + spam + "\ntotal nspam: " + notspam);
+}
+
+// get both files into memory as arrays
+var testData = readSync(trainingPath);
+var trainingData = readSync(testPath);
+classifyWholeSet(testData, trainingData, 5);
--- hw1.js
+++ hw1.js
@@ -1,7 +1,10 @@
+// TODO:
+// -have import statements passed in command line parameters
+// -get sorting working with euclidean distance
+
+
 var fs = require('fs');
 var csv = require('fast-csv');
-
-let counter = 0;
 
 // returns array containing csv data
 // path must be a string of format 'filename.csv'
@@ -10,21 +13,56 @@ var importAsList = function(path) {
   csv
     .fromPath(path)
     .on("data", function(data) {
-      counter++;
       fileData.push(data);
     })
     .on("end", function() {
       // console.log("Done. counter at: " + counter);
       //call next function
-      viewList(fileData);
+
+      calcDistance(fileData);
     });
 }
 
-var viewList = function (list) {
-  // for (var i = 0; i < list.length; i++) {
-  //   console.log(list[i]);
-  // }
-  console.log(list[1]);
+var calcDistance = function(fileData) {
+  var distances = [];
+  for (let i = 1; i < fileData.length; i++) {
+    distances.push([i]);
+    let nextDist = 0;
+    for (let j = 1; j < fileData[i].length-1; j++) {
+      nextDist+=parseInt(fileData[i][j]);
+    }
+    distances[i-1].push(nextDist);
+  }
+  distances.sort(distCalc);
+  decideOnDistance(fileData, distances);
 }
 
-importAsList('spam_test.csv');
+var distCalc = function (el1, el2) {
+  return el1[1] - el2[1];
+}
+
+// index 0 for not spam count, index 1 for spam count
+var decideOnDistance = function(fileData, distances, kchose = 3) {
+  let votes = [0,0];
+  for (var i = 0; i < kchose; i++) {
+    //console.log("secondary distance: " + distances[i][0].length-1);
+    let tempnext = parseInt(fileData[distances[i][0]][parseInt(fileData[distances[i][0]].length-1)]);
+    console.log(tempnext + '\n');
+    votes[tempnext]++;
+  }
+  console.log("votes (not spam, spam):\n"+votes);
+  return Math.max(votes[0], votes[1]);
+}
+
+
+var viewList = function (list) {
+  for (var i = 0; i < 10; i++) {
+    //console.log(list[i]);
+  }
+  // console.log(list);
+}
+
+var trainingPath = 'spam_train.csv';
+var testPath = 'spam_test.csv';
+
+importAsList(trainingPath);
